5842d08ea41085d5418403d26fee780aaf90cf00
Na224248 <fafuang@gmail.com>
2019-09-20 12:30:54 +0700

    - player move - edit Camera name script to Camera_1

--- Project 2/Assets/Scripts/Player/Player.cs
+++ Project 2/Assets/Scripts/Player/Player.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public Inventory inventory;
+
+    //moving
+    [SerializeField]
+    float moveSpeed;
+    public Vector3 target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //if (target.y > 2.0f) //set boundary for click to move
+            //{
+            //    target = transform.position;
+            //}
+            target.y = gameObject.transform.position.y;
+            target.z = gameObject.transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
     }
 }
--- Project 2/Assets/Scripts/other/Camera_1.cs
+++ Project 2/Assets/Scripts/other/Camera_1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camera_1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
